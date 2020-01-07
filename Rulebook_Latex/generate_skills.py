89d4eb4c43265e28460f62003282869dc95b6bfe
Alexander Lowry <Alexander.Lowry@interstates.com>
2020-01-07 10:03:38 -0600

    Changed "feelings" and "lasers" in skills list to "tech" and "sense", and had skills generator script conform to pep-8.

--- Rulebook_Latex/generate_skills.py
+++ Rulebook_Latex/generate_skills.py
@@ -1,20 +1,22 @@
 import pandas as pd
 import numpy as np
 
+
 def paragraph(header, content):
-    paragraph = "\n\n\\paragraph{" + header + "}\n"
-    paragraph += "\\hypertarget{Skill" + header.replace(" ", "") + "}{}\n"
-    paragraph += content
-    return paragraph
+    text = "\n\n\\paragraph{" + header + "}\n"
+    text += "\\hypertarget{Skill" + header.replace(" ", "") + "}{}\n"
+    text += content
+    return text
+
 
 list = pd.read_csv('skills.csv')
 list = np.array(list)
 
-#print(list)
+# print(list)
 
-list = list[list[:,0].argsort()]
+list = list[list[:, 0].argsort()]
 
-#print(list)
+# print(list)
 
 skills_feelings = []
 skills_lasers = []
@@ -22,17 +24,17 @@ abilities = []
 jokes = []
 
 for row in list:
-    if(row[2] == "feelings"):
+    if row[2] == "sense":
         skills_feelings.append(row[0:2])
-    if(row[2] == "lasers"):
+    if row[2] == "tech":
         skills_lasers.append(row[0:2])
-    if(row[2] == "ability"):
+    if row[2] == "ability":
         abilities.append(row[0:2])
-    if(row[2] == "joke"):
+    if row[2] == "joke":
         jokes.append(row[0:2])
 
 file_contents = "%Contains a list of all skills an abilities"
-file_contents +="\n%Each list should be in alphabetical order."
+file_contents += "\n%Each list should be in alphabetical order."
 file_contents += "\n%Generated based on data in 'skills.csv'"
 file_contents += "\n%And assembled via generate_skills.py"
 
@@ -43,7 +45,7 @@ file_contents += "\n\n\\subsection{\\feelingsC}"
 file_contents += "\n\\hypertarget{skillsFeelings}{}"
 for skill in skills_feelings:
     file_contents += paragraph(skill[0], skill[1])
-	
+
 
 file_contents += "\n\n\\subsection{\\lasersC}"
 file_contents += "\n\\hypertarget{skillsLasers}{}"
@@ -55,7 +57,7 @@ file_contents += "\n\\hypertarget{abilities}{}"
 for ability in abilities:
     file_contents += paragraph(ability[0], ability[1])
 
-#print(file_contents)
+# (file_contents)
 
 with open("skillslist.tex", "w") as text_file:
     text_file.write(file_contents)
@@ -64,7 +66,7 @@ print("Completed file 'skillslist.tex'.")
 
 
 joke_contents = "%Contains a list of all joke entries"
-joke_contents +="\n%List should be in alphabetical order."
+joke_contents += "\n%List should be in alphabetical order."
 joke_contents += "\n%Generated based on data in 'skills.csv'"
 joke_contents += "\n%And assembled via generate_skills.py"
 
